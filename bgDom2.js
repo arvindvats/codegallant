039a0bb680559e4ffef8ecabd283edd0270e0be3
arvindvats <74327803+arvindvats@users.noreply.github.com>
2022-02-22 14:52:25 +0530

    Update bgDom2.js

--- bgDom2.js
+++ bgDom2.js
@@ -5,7 +5,7 @@ var body = document.getElementById("gradient");
 var switchColor = document.getElementById("switchColor");
 var temp;
 color1.value = "#00ffcc";
-color2.value = "#7E14FF";
+color2.value = "#A600FF";
 
 // linear-gradient(to right, rgb(235, 255, 255), rgb(60, 144, 144)) // - default
 
@@ -33,12 +33,6 @@ function changeGradient(){
 }
 
 function switchGradient(){
-	// to make switch button gradient with backgroudn
-	// switchColor.style.background = "linear-gradient(to right, "
-	// +color1.value
-	// +", "
-	// +color2.value
-	// +")";
 	temp = color1.value;
 	color1.value = color2.value;
 	color2.value = temp;
